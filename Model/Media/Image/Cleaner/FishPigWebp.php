de255a60e64f24dcf44d39c5035d11735822af9a
Live Server <cwcs@fashionwallpaper.co.uk>
2026-03-10 18:11:04 +0000

    Add separate calls for webp subdirs

--- Model/Media/Image/Cleaner/FishPigWebp.php
+++ Model/Media/Image/Cleaner/FishPigWebp.php
@@ -14,24 +14,32 @@ class FishPigWebp extends AbstractCleaner
      */
     public function process(InputInterface $input, OutputInterface $output): void
     {
-        $basePath = BP . '/pub/media/fishpig/webp';
+        $basePaths = [
+            'pub/media/fishpig/webp/catalog' => 'pub/media/catalog',
+            'pub/media/fishpig/webp/wysiwyg' => 'pub/media/wysiwyg',
+            'pub/media/fishpig/webp/static' => 'pub/static'
+        ];
 
-        $this->recursiveScanDir(
-            $basePath,
-            function ($file) use ($input, $output) {
-                $originalFileWithoutExtension = preg_replace('/fishpig\/webp\/(.*)\.webp/', '$1', $file);
+        foreach ($basePaths as $basePath => $originalBasePath) {
+            $this->recursiveScanDir($basePath, function ($file) use ($input, $output, $basePath, $originalBasePath) {
+                $originalFileWithoutExtension = preg_replace(
+                    '/' . preg_quote($basePath, '/') . '\/(.*)\.webp/',
+                    $originalBasePath . '/$1',
+                    $file
+                );
 
                 foreach (['.jpg', '.jpeg', '.png'] as $extension) {
                     $originalFile = $originalFileWithoutExtension . $extension;
-                
+
                     if (is_file($originalFile)) {
                         $this->keepFile($input, $output, $file);
                         return;
                     }
                 }
+    
 
                 $this->removeFile($input, $output, $file);
-            }
-        );
+            });
+        }
     }
 }
